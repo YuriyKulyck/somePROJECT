2765f1ac208976152708b7a56b5480b568a77910
vladislav602 <kulycky@gmail.com>
2024-12-29 18:20:29 +0200

    partofPROJ

--- runtwo.py
+++ runtwo.py
@@ -3,4 +3,4 @@ import DBmanager
 dbmanager = DBmanager.DBManager("db_name.sql")
 dbmanager.create_tables()
 
-dbmanager.add_catalog(6, "Охолоджені та розливні напої", "Доставляються 24/7 з 100% цілісністю!")
+dbmanager.add_catalog(7, "Дитячі товари", "Знижки на дитячі іграшки та засоби -65%!")
