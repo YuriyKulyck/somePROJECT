aa322e089ee8c51adb8abd153531dca45714c015
sauceTartarys <kulycky@gmail.com>
2025-01-12 18:24:40 +0200

    tmpltContinue

--- DBmanager.py
+++ DBmanager.py
@@ -33,3 +33,9 @@ class DBManager:
         cursor.execute("INSERT INTO catalog(id, Title, Description) VALUES (?, ?, ?)", [id, title, description])
         self.connection.commit()
         cursor.close()
+
+    def add_article(self, id_of_article, id_catalog, Content, description, Price, Look, Connections, Location, Properties_and_signs):
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO Article(id_of_article, id_catalog, Content, description, Price, Look, Connections, Location, Properties_and_signs)) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", [id_of_article, id_catalog, Content, description, Price, Look, Connections, Location, Properties_and_signs])
+        self.connection.commit()
+        cursor.close()
