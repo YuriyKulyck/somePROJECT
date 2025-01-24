f1933b2b4f8cf1e25b969a169949f8596f311291
YuriyKulyck <kulycky@gmail.com>
2025-01-24 21:31:39 +0200

    SemiFINALofPROJ

--- run.py
+++ run.py
@@ -8,6 +8,12 @@ app.secret_key = "9919"
 @app.route("/")
 def index():
     return render_template("index.html")
+@app.route("/about_us")
+def about_us():
+    return render_template("about_us.html")
+@app.route("/confidence")
+def confidence():
+    return render_template("confidence.html")
 @app.route("/products_by_catagory/<id_category>")
 def product(id_category):
     dbmanager = DBmanager.DBManager("db_name.sql")
